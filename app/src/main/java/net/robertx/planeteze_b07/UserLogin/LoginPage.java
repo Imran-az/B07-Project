2e1c121672d1d534ec452705e3f19e924864d7e2
Imran-az <76496687+Imran-az@users.noreply.github.com>
2024-11-12 20:50:32 -0500

    created welcome page, sign up page, and login page

--- app/src/main/java/net/robertx/planeteze_b07/UserLogin/LoginPage.java
+++ app/src/main/java/net/robertx/planeteze_b07/UserLogin/LoginPage.java
@@ -1,21 +1,33 @@
-package net.robertx.planeteze_b07;
+package net.robertx.planeteze_b07.UserLogin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import net.robertx.planeteze_b07.R;
 
-public class MainActivity extends AppCompatActivity {
-
+public class LoginPage extends AppCompatActivity {
+    Button backbutton2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login_page);
 
+        backbutton2 = findViewById(R.id.backbutton_login);
+        backbutton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginPage.this, WelcomePage.class);
+                startActivity(intent);
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
