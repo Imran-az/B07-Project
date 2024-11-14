f6dd3fdedb65dc76e167fa3debf05cdedf6a1e90
Robert <9291792+bushshrub@users.noreply.github.com>
2024-11-13 19:39:09 -0500

    feat: Create activity for carbon footprint questionnaire
    Create activity for carbon footprint questionnaire, added a temporary button to the main activity to access it

--- app/src/main/java/net/robertx/planeteze_b07/CarbonFootprintQuestionnaire.java
+++ app/src/main/java/net/robertx/planeteze_b07/CarbonFootprintQuestionnaire.java
@@ -1,8 +1,8 @@
 package net.robertx.planeteze_b07;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,24 +10,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class CarbonFootprintQuestionnaire extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_carbon_footprint_questionnaire);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, CarbonFootprintQuestionnaire.class);
-            startActivity(intent);
-        });
+        Spinner countrySpinner = findViewById(R.id.countrySpinner);
 
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.country_list, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        countrySpinner.setAdapter(adapter);
+        countrySpinner.setSelection(31);
     }
 }
