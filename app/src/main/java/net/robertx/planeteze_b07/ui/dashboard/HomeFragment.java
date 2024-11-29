cdedad30196e8c96618b0d5ea9cea640963f7197
Robert <9291792+bushshrub@users.noreply.github.com>
2024-11-29 14:08:05 -0500

    feat: add button to view annual carbon footprint survey results

--- app/src/main/java/net/robertx/planeteze_b07/ui/dashboard/HomeFragment.java
+++ app/src/main/java/net/robertx/planeteze_b07/ui/dashboard/HomeFragment.java
@@ -1,5 +1,6 @@
 package net.robertx.planeteze_b07.ui.dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,8 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import net.robertx.planeteze_b07.R;
+import net.robertx.planeteze_b07.annualCarbonTracker.ResultsActivity;
+import net.robertx.planeteze_b07.annual_carbon_footprint.AnnualCarbonFootprintSurvey;
 
 public class HomeFragment extends Fragment {
 
@@ -17,6 +20,17 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        view.findViewById(R.id.cardView).setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), AnnualCarbonFootprintSurvey.class);
+            startActivity(intent);
+        });
+
+        view.findViewById(R.id.cardViewResults).setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), ResultsActivity.class);
+            startActivity(intent);
+        });
+
         return view;
     }
 }
