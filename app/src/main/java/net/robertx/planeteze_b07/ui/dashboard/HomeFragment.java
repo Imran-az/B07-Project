efe8ed733eb150822d2d252ebfc245f4dfb84f29
Robert <9291792+bushshrub@users.noreply.github.com>
2024-11-29 14:30:43 -0500

    feat: show UI elements that tell the user what they can or cannot perform

--- app/src/main/java/net/robertx/planeteze_b07/ui/dashboard/HomeFragment.java
+++ app/src/main/java/net/robertx/planeteze_b07/ui/dashboard/HomeFragment.java
@@ -10,11 +10,19 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import net.robertx.planeteze_b07.R;
 import net.robertx.planeteze_b07.annualCarbonTracker.ResultsActivity;
 import net.robertx.planeteze_b07.annual_carbon_footprint.AnnualCarbonFootprintSurvey;
 
 public class HomeFragment extends Fragment {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    FirebaseAuth auth = FirebaseAuth.getInstance();
 
     @Nullable
     @Override
@@ -26,7 +34,24 @@ public class HomeFragment extends Fragment {
             startActivity(intent);
         });
 
-        view.findViewById(R.id.cardViewResults).setOnClickListener(v -> {
+        view.findViewById(R.id.cardViewSurveyResults).setEnabled(false);
+
+        if (auth.getCurrentUser() != null) {
+            CollectionReference collection = db.collection("AnnualCarbonFootprintSurveyData");
+            // check if document with path auth.getCurrentUser().getUid() exists
+            collection.document(auth.getCurrentUser().getUid()).get().addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    if (task.getResult().exists()) {
+                        view.findViewById(R.id.textAlreadyTakenSurvey).setVisibility(View.VISIBLE);
+                        view.findViewById(R.id.textCantViewResultsWithoutTakingSurvey).setVisibility(View.INVISIBLE);
+                        view.findViewById(R.id.cardViewSurveyResults).setEnabled(true);
+                    }
+                }
+            });
+        }
+
+
+        view.findViewById(R.id.cardViewSurveyResults).setOnClickListener(v -> {
             Intent intent = new Intent(getActivity(), ResultsActivity.class);
             startActivity(intent);
         });
